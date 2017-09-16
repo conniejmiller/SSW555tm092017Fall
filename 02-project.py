f300503127951b7307f8ebeea38b832e654db1fa
conniejmiller <conniejmiller@yahoo.com>
2017-09-15 22:27:18 -0500

    Update 02-project.py
    not working yet....but maybe closer....

--- 02-project.py
+++ 02-project.py
@@ -6,27 +6,49 @@ def valid_tag(level,tag):
                   "2": ["DATE"]}
                   
     return "Y" if level in valid_tags and tag in valid_tags[level] else "N"
-        
     
 # print the formatted line
 def print_line(level, tag, args):
     print("<-- %s|%s|%s|%s" % (level, tag, valid_tag(level, tag), args))
 
+# read the file and process the rows  
+fp = open('01-project.ged', encoding='utf-8')
 
-# read the file and process the rows
-with open('01-project.ged', encoding='utf-8') as f:
-    for line in f:
-        print("-->", line.strip())
-        words = line.split()  # split() splits on whitespace and eliminates trailing whitespace
-        if len(words) >= 2:
-            level = words[0]
-            tag = words[1]
-            
-            if len(words) == 3 and words[2] in ("INDI", "FAM"):
-                print_line(level, words[2], words[1])
+individual = []
+family = []
+    
+while 1:
+    line = fp.readline()
+    if not line:
+        break
+    words = line.split()  # split() splits on whitespace and eliminates trailing whitespace
+
+    if len(words) >= 2:
+        level = words[0]
+        tag = words[1]
+        other_stuff = words[2:]
+   
+        if len(words) == 3 and words[2] in ("INDI", "FAM"):
+                
+            if words[2] == "INDI":
+                indi_dict = {"ID":tag,"NAME":'',"SEX":'',"BIRT":'',"DEAT":''}
+                this_type = 'INDI'
+                individual.append(indi_dict)
             else:
-                print_line(level, tag, " ".join(words[2:])) # create a string from a sequence of words starting at words[2] separated by " "
-        else:
-            # badly formatted line
-            print_line("?", "?", "?")
+                fam_dict = {"ID":tag,"MARR":'',"HUSB":'',"WIFE":'',"CHIL":[],"DIV":''}
+                this_type = 'FAM'
+                family.append(fam_dict)
             
+        else:
+            if this_type == 'INDI':
+                print ("INDI")
+            else:
+                print ("FAM")
+
+    else:
+        # badly formatted line
+        print_line("?", "?", "?")
+
+print (individual)
+print ('')
+print (family)
