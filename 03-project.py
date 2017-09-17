f5c70dc1a93c449aa3cdb74a744f151414ef2da8
conniejmiller <conniejmiller@yahoo.com>
2017-09-17 11:20:45 -0500

    Update 03-project.py
    restored process_file logic and corrected output

--- 03-project.py
+++ 03-project.py
@@ -11,119 +11,139 @@ def valid_tag(level,tag):
 def print_line(level, tag, args):
     print("<-- %s|%s|%s|%s" % (level, tag, valid_tag(level, tag), args))
 
-# read the file and process the rows  
-fp = open('01-project_Miller.ged', encoding='utf-8')
-
-individual = []
-family = []
-    
-this_type = 'new'
-
-while 1:
-    line = fp.readline()
-    if not line:
-        break
-    words = line.split()  # split() splits on whitespace and eliminates trailing whitespace
-
-    if len(words) >= 2:
-        level = words[0]
-        tag = words[1]
-        other_stuff = words[2:]
-   
-        if len(words) == 3 and words[2] in ("INDI", "FAM"):
-            # print lastline
-            if this_type == 'INDI':
-                individual.append(indi_dict)
-            elif this_type == 'FAM':
-                family.append(fam_dict)
-
-            if words[2] == "INDI":
-                indi_dict = {"ID":tag,"NAME":'',"SEX":'',"BIRT":'',"DEAT":''}
-                this_type = 'INDI'
-            else:
-                fam_dict = {"ID":tag,"MARR":'',"HUSB":'',"WIFE":'',"CHIL":[],"DIV":''}
-                this_type = 'FAM'
-        
-        elif tag in ("NAME","SEX"):
-            indi_dict[tag] = " ".join(other_stuff)
-            
-        elif tag in ("BIRT","DEAT"):
-            line = fp.readline()
-            words = line.split()
-            indi_dict[tag] = " ".join(words[2:])
-            
-        elif tag in ("MARR","DIV"):
-            line = fp.readline()
-            words = line.split()
-            fam_dict[tag] = " ".join(words[2:])
-            
-        elif tag in ("HUSB","WIFE"):
-            fam_dict[tag] = " ".join(other_stuff)
-            
-        elif tag == "CHIL":
-            fam_dict[tag].append(words[2])
-
-    else:
-        # badly formatted line
-        print_line("?", "?", "?")
-
-# now print the last one
-if this_type == 'INDI':
-    individual.append(indi_dict)
-elif this_type == 'FAM':
-    family.append(fam_dict)
-
-#now print output     
-from operator import itemgetter
-
-def getname(id):
-    for row in individual:
+# get name for an individual
+def getname(list, id):
+    for row in list:
         if row["ID"] == id:
             return row["NAME"]
     return "Unknown"
+
+# process rows in the matrix
+def process_words(wordMatrix):
+    this_type = 'new'
+    this_tag = 'new'
+    individual = []
+    family = []
+    indi_dict = {}
+    family_dict = {}
+
+    for words in wordMatrix:
+        if len(words) >= 2:
+            level = words[0]
+            tag = words[1]
+            other_stuff = words[2:]
+
+            if len(words) == 3 and words[2] in ("INDI", "FAM"):
+                # print lastline
+                if this_type == 'INDI':
+                    individual.append(indi_dict)
+                elif this_type == 'FAM':
+                    family.append(fam_dict)
+
+                if words[2] == "INDI":
+                    indi_dict = {"ID": tag, 
+                                 "NAME": '', 
+                                 "SEX": '', 
+                                 "BIRT": '', 
+                                 "DEAT": ''}
+                    this_type = 'INDI'
+                else:
+                    fam_dict = {"ID": tag, 
+                                "MARR": '', 
+                                "HUSB": '', 
+                                "WIFE": '', 
+                                "CHIL": [], 
+                                "DIV": ''}
+                    this_type = 'FAM'
+            
+            elif tag in ("NAME", "SEX"):
+                indi_dict[tag] = " ".join(other_stuff)
+                
+            elif tag in ("BIRT", "DEAT", "MARR", "DIV"):
+                #savethis tag so we can write it after the next row
+                this_tag = tag
+                
+            elif tag in ("DATE"):
+                if this_type == 'INDI':
+                    indi_dict[this_tag] = " ".join(other_stuff)
+                elif this_type == 'FAM':
+                    fam_dict[this_tag] = " ".join(other_stuff)
+                this_tag = 'new'
+            
+            elif tag in ("HUSB", "WIFE"):
+                fam_dict[tag] = " ".join(other_stuff)
+            
+            elif tag == "CHIL":
+                fam_dict[tag].append(words[2])
+
+    # now print the last one
+    if this_type == 'INDI':
+        individual.append(indi_dict)
+    elif this_type == 'FAM':
+        family.append(fam_dict)
+
+    return individual, family
+
+# process the file
+def process_file():
+    FILE_NAME = '01-project_Miller.ged'
+    words = []
+
+    # read the file and process the rows
+    with open(FILE_NAME) as inFile:
+        for line in inFile:
+            words.append(line.split())
+
+    individual, family = process_words(words)
     
-for row in sorted(individual, key=itemgetter("ID")):
-    print(row["ID"] + " : " + row["NAME"])
+    #print(individual)
+    #print(family)
 
-for row in sorted(family, key=itemgetter("ID")):
-    print (row["ID"] + " : " + row["HUSB"] + ":" + getname(row["HUSB"]) + " and " + row["WIFE"] + ":" + getname(row["WIFE"]))
-
+    #now print output     
+    from operator import itemgetter          
+    for row in sorted(individual, key=itemgetter("ID")):
+        print(row["ID"] + " : " + row["NAME"])
     
+    for row in sorted(family, key=itemgetter("ID")):
+        print (row["ID"] + " : " + row["HUSB"] + ":" + getname(individual,row["HUSB"]) + " and " + row["WIFE"] + ":" + getname(individual,row["WIFE"]))
+    
+    # now try in table with all data in a table
+    from prettytable import PrettyTable
+    
+    individuals = PrettyTable(["ID", 
+                               "NAME", 
+                               "GENDER", 
+                               "BIRTHDAY",
+                               "DEATH"])
+    individuals.padding_width = 1 # One space between column edges and contents (default)
+    individuals.align["NAME"] = "l" # Left align names
+    for row in sorted(individual, key=itemgetter("ID")):
+        individuals.add_row([row["ID"], 
+                             row["NAME"],
+                             row["SEX"],
+                             row["BIRT"],
+                             row["DEAT"]])
+    print (individuals)
+    
+    families = PrettyTable(["ID", 
+                            "MARRIED",
+                            "DIVORCED",
+                            "HUSBAND", 
+                            "WIFE",
+                            "CHILDREN"])
+    families.padding_width = 1 # One space between column edges and contents (default)
+    families.align["HUSBAND"] = "1"
+    families.align["WIFE"] = "1"
+    
+    for row in sorted(family, key=itemgetter("ID")):
+        families.add_row([row["ID"],
+                          row["MARR"],
+                          row["DIV"],
+                          row["HUSB"]+ ":" + getname(individual,row["HUSB"]), 
+                          row["WIFE"] + ":" + getname(individual,row["WIFE"]),
+                          row["CHIL"]])
+    print (families)
 
-# now try in table with all data in a table
-from prettytable import PrettyTable
-
-individuals = PrettyTable(["ID", 
-                           "NAME", 
-                           "GENDER", 
-                           "BIRTHDAY",
-                           "DEATH"])
-individuals.padding_width = 1 # One space between column edges and contents (default)
-individuals.align["NAME"] = "l" # Left align names
-for row in sorted(individual, key=itemgetter("ID")):
-    individuals.add_row([row["ID"], 
-                         row["NAME"],
-                         row["SEX"],
-                         row["BIRT"],
-                         row["DEAT"]])
-print (individuals)
-
-families = PrettyTable(["ID", 
-                        "MARRIED",
-                        "DIVORCED",
-                        "HUSBAND", 
-                        "WIFE",
-                        "CHILDREN"])
-families.padding_width = 1 # One space between column edges and contents (default)
-families.align["HUSBAND"] = "1"
-families.align["WIFE"] = "1"
-
-for row in sorted(family, key=itemgetter("ID")):
-    families.add_row([row["ID"],
-                      row["MARR"],
-                      row["DIV"],
-                      row["HUSB"]+ ":" + getname(row["HUSB"]), 
-                      row["WIFE"] + ":" + getname(row["WIFE"]),
-                      row["CHIL"]])
-
-print (families)
+if __name__ == '__main__':
+    process_file()
+    
