6fe172439d46aff3cea7b81561267e1b67d40812
vporta <vporta7@gmail.com>
2017-09-22 20:55:15 -0400

    US29 list deceased individuals. Created two functions and tests

--- test.py
+++ test.py
@@ -8,13 +8,20 @@ class TestEmptyInput(unittest.TestCase):
         wordMatrix = [[]]
         self.assertEqual(project.process_words(wordMatrix), ([], []))
 
-    def test_dateCompare(self):
-        self.assertEqual(dateCompare('10 SEP 2017'), 'True')
-        self.assertTrue(dateCompare('10 SEP 2017'))
-        self.assertEqual(dateCompare('10 SEP 2018'), 'False')
-        self.assertEqual(dateCompare('20 SEP 2017'), 'False')
-        self.assertNotEqual(dateCompare('10 SEP 2017'), 'False')
-        self.assertNotEqual(dateCompare('10 SEP 2018'), 'True')
-        
+    def test_date_compare(self):
+        self.assertEqual(project.date_compare('10 SEP 2017'), True)
+        self.assertTrue(project.date_compare('10 SEP 2017'), True)
+        self.assertEqual(project.date_compare('10 SEP 2018'), False)
+        self.assertEqual(project.date_compare('20 SEP 2017'), True)
+        self.assertNotEqual(project.date_compare('10 SEP 2018'), True)
+
+    def test_is_deceased(self):
+        self.assertEqual(project.is_deceased(''), False) 
+        self.assertEqual(project.is_deceased('11 SEP 1998'), True)
+        self.assertEqual(project.is_deceased('18 MAR 2007'), True)
+        self.assertFalse(project.is_deceased(''), True)
+        self.assertTrue(project.is_deceased('18 MAR 2007'), True)
+
+
 if __name__ == '__main__':
     unittest.main()
